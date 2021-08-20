9a3c20a22cbc81d6552b7eafec20a1f71cd7699f
JaeHyun <soehd0889@naver.com>
2021-08-20 19:48:25 +0900

    [Greedy] 큰 수의 법칙

---
+++ code/Greedy/3-2.py
@@ -0,0 +1,24 @@
+n,m,k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+first = arr[n-1]
+second = arr[n-2]
+
+sum = 0
+
+while True:
+    for j in range(k):
+        if m == 0:
+            break
+        sum += first
+        m -= 1
+    if m == 0:
+        break
+    sum += second
+    m -= 1
+
+print(sum)
+
