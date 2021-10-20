18ee10898f41d4b2c9d29226ea72239e633f6b94
JaeHyun <soehd0889@naver.com>
2021-10-20 19:26:55 +0900

    [완전탐색] 피타고라스

--- 프로그래머스 python/피타고라스.py
+++ 프로그래머스 python/피타고라스.py
@@ -5,6 +5,5 @@ for c in range(1000):
             if a+b+c != 1000:
                 continue
             if a**2 + b**2 == c**2:
-                break;
-
-print(a*b*c)
+                print(a*b*c)
+                break
