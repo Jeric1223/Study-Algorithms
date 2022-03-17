0518e2ec822a66928177a6fd55758795a3b0804a
JaeHyun <soehd0889@naver.com>
2022-03-17 11:28:18 +0900

    기온 데이터 분석하기

--- code/인공지능 프로그래밍/기온 데이터 분석하기.py
+++ code/인공지능 프로그래밍/기온 데이터 분석하기.py
@@ -1,9 +1,18 @@
 import csv
+import numpy as np
 
 with open('data.csv','r') as file:
     data = csv.reader(file,delimiter=',')
+    """header = next(data)
+    print(type(header))"""
+    maxArr = []
     arr = []
     for row in data:
         arr.append(row[0:5])
-        print(row[0:5])
-    print(len(arr))
+        if row[4] == '':
+            row[4] = -99
+        maxArr.append(float(row[4]))
+
+    maxvalue = max(maxArr)
+    maxIndex = maxArr.index(maxvalue)
+    print(f'서울 {arr[maxIndex][0]} 일에 역대급 최고온도 {maxvalue}')
