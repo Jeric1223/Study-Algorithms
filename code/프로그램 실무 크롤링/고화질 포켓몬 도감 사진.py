08ce5616efbb2c27defb7f37709580d32e1e502f
JaeHyun <soehd0889@naver.com>
2022-06-30 09:07:35 +0900

    코드를 알기 쉽게 주석처리

--- code/프로그램 실무 크롤링/고화질 포켓몬 도감 사진.py
+++ code/프로그램 실무 크롤링/고화질 포켓몬 도감 사진.py
@@ -17,25 +17,24 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
+
+    # image를 추출할 select와 이름을 추출할 select
     imageDiv = soup.select('#content > div.inner > div.entry-content > div > table > tbody > tr > td:nth-child(3) > figure > span > img')
     poketmonName = soup.select('#content > div.inner > div.entry-content > div > table > tbody > tr > td:nth-child(2)')
 
+    # 추출한 poketmonName 리스트를 반복문을 통해서 가공하는 코드
     for index in range(1,len(poketmonName)+1):
+
+        # (2개)라는 데이터의 속성을 알기위해서 테스트 코드
         if "(2개)" in poketmonName[index].text:
             print(poketmonName[index].text)
 
         print(poketmonName[index].text[0:-4])
+        # 포켓몬 씰중에 중복되는 포켓몬 들이 있기 때문에 만약에 (2개)라고 되어있으면 (2개)라는 부분을 삭제하는 코드
         if poketmonName[index].text[-1:-5] == "(2개)":
             name_list.append(poketmonName[index].text[0:-4])
         name_list.append(poketmonName[index].text)
 
     print(poketmonName[1].text)
-
-    for item in map(lambda x: x['src'], imageDiv[:-1]):
-        start = time.time()
-        print(item)
-        os.system(f"curl {item} > {item.split('/')[-1]}")
-        print(time.time() - start)
-        img = Image.open(item.split('/')[-1])
 else:
     print(response.status_code)
