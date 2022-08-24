1021a92c56f14d25b5a9dfc33a9dee9d00a1705d
JaeHyun <soehd0889@cntech.pro>
2022-08-24 17:39:55 +0900

    2022 KAKAO TECH INTERNSHIP - 성격 유형 검사하기

---
+++ 프로그래머스 Javascript/성격 유형 검사하기.js
@@ -0,0 +1,31 @@
+function solution(survey, choices) {
+  const surveyOrder = ["R", "T", "C", "F", "J", "M", "A", "N"];
+  const surveyScore = Array(8).fill(0);
+  let answer = "";
+
+  survey.forEach((item, index) => {
+    let surveyType = item.split("");
+    if (choices[index] - 4 < 0) {
+      surveyScore[surveyOrder.indexOf(surveyType[0])] += Math.abs(
+        choices[index] - 4
+      );
+    } else if (choices[index] - 4 > 0) {
+      surveyScore[surveyOrder.indexOf(surveyType[1])] += choices[index] - 4;
+    }
+  });
+
+  surveyScore.forEach((_item, i, arr) => {
+    if (i % 2 == 0) {
+      if (arr[i] > arr[i + 1]) {
+        answer += surveyOrder[i];
+      } else if (arr[i] < arr[i + 1]) {
+        answer += surveyOrder[i + 1];
+      } else {
+        let concatstring = surveyOrder[i] + surveyOrder[i + 1];
+        answer += surveyOrder[i];
+      }
+    }
+  });
+
+  return answer;
+}
